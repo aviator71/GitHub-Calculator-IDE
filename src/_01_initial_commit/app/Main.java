4498ef8ba63d3b7eb801552ba41ee5266d0ca0c5
aviator71 <aviator71@ukr.net>
2022-12-26 18:47:07 +0200

    Add app description

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Super app!");
+        System.out.println("Calculator for base calculations");
         int a = 8;
         int b = 2;
         int c = add(a, b);
         System.out.println(c);
     }
     private static int add(int a, int b) {
+
         return a + b;
     }
 }
