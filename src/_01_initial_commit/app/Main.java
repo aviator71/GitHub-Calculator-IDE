f092c03286fc4b8ef7ac14e81abd88b9545a0ea3
aviator71 <aviator71@ukr.net>
2022-12-26 18:32:31 +0200

    Update app info

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -3,6 +3,6 @@ package _01_initial_commit.app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("My git app. Super!");
     }
 }
